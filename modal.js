b5f2e6947ee47207a3c6d3f9eaf5f259a968ae47
FerreiraWillianw <willian.ferreiraw30@gmail.com>
2026-09-22 17:26:38 -0300

    quase finalizacao do projeto

--- modal.js
+++ modal.js
@@ -1,10 +1,50 @@
 
 const background = document.getElementById('modal-background');
+const modalContainer = document.getElementById('modal-container');
 
-overlay.classList.add('open');
+let currentMovie = {};
 
 function backgroundClickHandler() {
     overlay.classList.remove('open')
 }
+
+function addCurrentMovieToList() {
+    if (isMovieAlreadyOnList(currentMovie.imdbID)){
+        notie.alert({ type: "error", text: "Filme já está na sua lista!"});
+        return;
+    }
+    addToList(currentMovie);
+    updateUI(currentMovie);
+    closeModal();
+}
+
+function createModal(data) {
+    currentMovie = data;
+
+    modalContainer.innerHTML = `
+         <h2 id="movie-title">${data.Title} - ${data.Year}</h2>
+            <section id="modal-body">
+                <img src="${data.Poster}" id="movie-poster" alt="Poster do filme">
+                <div id="movie-info">
+                    <h3 id="movie-plot">
+                        ${data.Plot}
+                    </h3>
+                    <div id="movie-cast">
+                        <h4>Elenco: </h4>
+                        <h5>${data.Actors}</h5>
+                    </div>
+                    <div id="movie-genre">
+                        <h4>Gênero:</h4>
+                        <h5>${data.Genre}</h5>
+                    </div>
+                </div>
+            </section>
+            <section id="modal-footer">
+                <button id="add-to-list" onclick="addCurrentMovieToList()">Adicionar à Lista</button>
+            </section>
+    `
+}
+
+
 
 background.addEventListener('click', backgroundClickHandler)
